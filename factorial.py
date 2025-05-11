180411ef9e3ca82c40e2fb29644dd920e585c3a5
Sachin Kumar <sachinsisodiya005@gmail.com>
2025-05-11 23:19:55 +0530

    factorial.py
    factorial using for and while loop

---
+++ factorial.py
@@ -0,0 +1,15 @@
+# Factorial of a number using for loop
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1, n+1):
+#     fact *=i
+# print(fact)
+
+# Factorial of a number using while loop 
+num = int(input("Enter a number: "))
+fact = 1
+i = 1
+while i <= num:
+    fact *= i
+    i += 1
+print(fact) 
